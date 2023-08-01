84c5e42ccf6171682154bdec2ebaa3cb2868b573
himanshurohit11 <95362244+himanshurohit11@users.noreply.github.com>
2023-08-01 23:58:38 +0530

    Update MyCalculator.java

--- src/main/java/mypkg/MyCalculator.java
+++ src/main/java/mypkg/MyCalculator.java
@@ -23,6 +23,7 @@ public class MyCalculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyCalculator ob = new MyCalculator();
+		System.out.println("This is my First Maven with Jenkins Project");
 		System.out.println("Addition is" + ob.sum(10, 20));
 		System.out.println("Substraction is" + ob.sub(30, 20));
 		System.out.println("Multiplition is" + ob.mul(30, 20));
