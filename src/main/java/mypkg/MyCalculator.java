87840f4b0b8ee856a8df6fda9bedbe82b9086f2c
himanshurohit11 <himanshurohti114@gmail.com>
2023-08-01 23:22:04 +0530

    Added division function

--- src/main/java/mypkg/MyCalculator.java
+++ src/main/java/mypkg/MyCalculator.java
@@ -15,12 +15,18 @@ public class MyCalculator {
 		return a*b;
 	}
 
+	public float div(int a, int b)
+	{
+		return a*b;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MyCalculator ob = new MyCalculator();
 		System.out.println("Addition is" + ob.sum(10, 20));
 		System.out.println("Substraction is" + ob.sub(30, 20));
 		System.out.println("Multiplition is" + ob.mul(30, 20));
+		System.out.println("Division is" + ob.div(30, 2));
 	}
 
 }
